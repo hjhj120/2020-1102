57a768cca486b29158c0e3db0703207ae4351f6c
hjhj120 <54790476+hjhj120@users.noreply.github.com>
2020-11-02 08:12:27 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	
+	var a = $(".box")
+	
+	$.each(a. function(i,d){
+
+		console.log(i, d)
+		$(d).css ("width", "100px")
+		$(d).css ("height", "100px")
+		$(d).css ("background-color","red")
+	})
+
+
+})
